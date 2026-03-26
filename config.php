0a29673ad3233e3b816342f0af06a43f27f2dd75
INGRID VITORIA SOARES SILVA <ingrid.vssilva2@senacsp.edu.br>
2026-03-26 19:39:06 -0300

    Projeto crud-php - Listar e Excluir - ok

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+    $dbname = 'crud_php';
+    $host = 'localhost:4307'; 
+    $dbuser = 'root';
+    $dbpass = '';
+
+    try{
+        $db = new PDO("mysql:dbname=".$dbname.";host=" .$host,$dbuser,$dbpass);
+
+    } catch(PDOException $e){
+    echo "Erro na conexão" . $e->getMessage();
+        exit();
+    }
+    define('BASE_URL','http://localhost/crud_php/')
+
+?>
+
+
+
+
+
+
+
+
+
+
+
